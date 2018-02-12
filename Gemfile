4a5f87e3280b9fadf4b1cdc0ca519d280ca91d76
jdevine82 <jdevine82@gmail.com>
2018-02-12 05:36:38 +0000

    s3 migration 1

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'ransack'
 gem 'bootstrap-sass'
 gem 'autoprefixer-rails'
 gem 'puma', '~> 3.0'
+gem 'aws-sdk',   '~> 2.6'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+    config.paperclip_defaults = {
+        storage: :s3,
+        s3_credentials: {
+          bucket: ENV['dbs504'],
+          access_key_id: ENV['AKIAJXSAWPWRWIXZR4NAD'],
+          secret_access_key: ENV['ibuBiOOx7eZMKOgNraXeFxrnVJyV357h6qb51rIo'],
+          s3_region: ENV['N. Virginia'],
+          
+        }
+    }
 end
